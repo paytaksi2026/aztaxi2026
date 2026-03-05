942e1a76d21ecd9efb11a10bd11d73df4a2e1156
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-05 13:13:24 +0400

    Add files via upload

---
+++ public/js/map_v4.js
@@ -0,0 +1,21 @@
+
+var map=L.map('map').setView([40.4093,49.8671],13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+maxZoom:19
+}).addTo(map);
+
+var marker=L.marker([40.4093,49.8671]).addTo(map);
+
+function calcRoute(){
+
+var distance=(Math.random()*6+1).toFixed(2);
+document.getElementById("distance").innerText=distance;
+
+var price=3.5;
+if(distance>3){
+price+=(distance-3)*0.30;
+}
+
+document.getElementById("price").innerText=price.toFixed(2);
+}
