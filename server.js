962941a56cc501d978af70bcffe4186d22eadb65
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-11 21:03:32 +0400

    Update server.js

--- server.js
+++ server.js
@@ -9,53 +9,26 @@ const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.json())
-
-// static folders
 app.use("/passenger", express.static(path.join(__dirname,"passenger")))
 app.use("/driver", express.static(path.join(__dirname,"driver")))
 app.use("/admin", express.static(path.join(__dirname,"admin")))
 
 let drivers = []
-let orders = []
 
 io.on("connection",(socket)=>{
 
-console.log("client connected")
-
-// DRIVER LOCATION
 socket.on("driver-location",(data)=>{
 
 let driver = drivers.find(d=>d.id===data.id)
 
 if(!driver){
-
 drivers.push(data)
-
 }else{
-
 driver.lat = data.lat
 driver.lng = data.lng
-
 }
 
 io.emit("drivers-update",drivers)
-
-})
-
-// PASSENGER ORDER
-socket.on("create-order",(order)=>{
-
-orders.push(order)
-
-io.emit("new-order",order)
-
-})
-
-// DRIVER ACCEPT
-socket.on("accept-order",(data)=>{
-
-io.emit("order-accepted",data)
 
 })
 
@@ -66,5 +39,5 @@ res.send("AzTaxi server işləyir")
 })
 
 server.listen(PORT,()=>{
-console.log("AzTaxi realtime server running " + PORT)
+console.log("AzTaxi realtime server started")
 })
