20cd515f49326bb6acde466f76c21741c80b28d3
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-12 00:03:47 +0400

    Add files via upload

--- assets/js/passenger.js
+++ assets/js/passenger.js
@@ -6,7 +6,7 @@ const map = L.map('map').setView([40.4093,49.8671],13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 let pickupMarker;
-let driverMarkers = {};
+let driverMarkers={};
 
 map.on("click",(e)=>{
 if(pickupMarker) map.removeLayer(pickupMarker);
@@ -21,17 +21,10 @@ alert("Ride requested");
 
 socket.on("drivers",(drivers)=>{
 
-Object.keys(driverMarkers).forEach(id=>{
-if(!drivers[id]){
-map.removeLayer(driverMarkers[id]);
-delete driverMarkers[id];
-}
-});
-
 Object.entries(drivers).forEach(([id,d])=>{
 
 if(!driverMarkers[id]){
-driverMarkers[id] = L.marker([d.lat,d.lng]).addTo(map);
+driverMarkers[id]=L.marker([d.lat,d.lng]).addTo(map);
 }else{
 driverMarkers[id].setLatLng([d.lat,d.lng]);
 }
