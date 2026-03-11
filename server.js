1613bda2bacd2ad5d15878bf036a8b512002ba96
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-11 21:23:27 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,42 @@
-const express = require("express")
-const http = require("http")
-const socketio = require("socket.io")
-const path = require("path")
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use("/passenger", express.static(path.join(__dirname,"passenger")))
-app.use("/driver", express.static(path.join(__dirname,"driver")))
-app.use("/admin", express.static(path.join(__dirname,"admin")))
+app.use("/driver", express.static(path.join(__dirname, "driver")));
+app.use("/passenger", express.static(path.join(__dirname, "passenger")));
 
-let drivers = []
+let drivers = [];
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
-socket.on("driver-location",(data)=>{
+  socket.on("driver-location", (data) => {
 
-let driver = drivers.find(d=>d.id===data.id)
+    let driver = drivers.find(d => d.id === data.id);
 
-if(!driver){
-drivers.push(data)
-}else{
-driver.lat = data.lat
-driver.lng = data.lng
-}
+    if (!driver) {
+      drivers.push(data);
+    } else {
+      driver.lat = data.lat;
+      driver.lng = data.lng;
+    }
 
-io.emit("drivers-update",drivers)
+    io.emit("drivers-update", drivers);
 
-})
+  });
 
-})
+});
 
-app.get("/",(req,res)=>{
-res.send("AzTaxi server işləyir")
-})
+app.get("/", (req,res)=>{
+  res.send("AzTaxi server işləyir");
+});
 
-server.listen(PORT,()=>{
-console.log("AzTaxi realtime server started")
-})
+server.listen(PORT, ()=>{
+  console.log("AzTaxi server running");
+});
