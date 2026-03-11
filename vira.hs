223e99b953dcd3010dfe145ba6739206d4df7de4
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-11 16:04:29 +0400

    Delete vira.hs

--- vira.hs
+++
@@ -1,7 +0,0 @@
--- Pipeline configuration for Vira <https://vira.nixos.asia/>
-
-\ctx pipeline ->
-  pipeline
-    { signoff.enable = False
-    , cache.url = Just "https://cache.nixos.asia/oss"
-    }
