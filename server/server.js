ca219cdbdfbb8fd6a25fe593db098cd5ffb68c96
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-13 01:00:55 +0400

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(express.static("public"));
 app.use(express.json());
 
 let drivers={};
+let rides={};
 
 function distance(a,b){
  const dx=a.lat-b.lat;
@@ -44,8 +45,12 @@ io.on("connection",socket=>{
 
  });
 
+ socket.on("ride-accept",ride=>{
+   io.emit("ride-accepted",ride);
+ });
+
 });
 
 server.listen(process.env.PORT||3000,()=>{
- console.log("AzTaxi V6 running");
+ console.log("AzTaxi V7 running");
 });
