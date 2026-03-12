35a8af9d43303d36446bcc32157288dd8b9f9765
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-12 15:47:50 +0400

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE drivers(
+id SERIAL PRIMARY KEY,
+name TEXT,
+car TEXT,
+status TEXT
+);
+
+CREATE TABLE rides(
+id SERIAL PRIMARY KEY,
+pickup TEXT,
+dropoff TEXT,
+package TEXT,
+status TEXT
+);
