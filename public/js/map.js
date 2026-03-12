d2f81c6445b94318c02ac7fe4a36589979f5d228
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-12 23:26:17 +0400

    Add files via upload

---
+++ public/js/map.js
@@ -0,0 +1,8 @@
+
+var map = L.map('map').setView([40.4093,49.8671], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+}).addTo(map);
+
+var marker = L.marker([40.4093,49.8671]).addTo(map);
