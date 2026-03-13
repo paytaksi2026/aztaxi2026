913795577f14fe9535d90f9f9812a15c9a38ff97
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-13 17:45:56 +0400

    Add files via upload

--- public/js/passenger.js
+++ public/js/passenger.js
@@ -1,14 +1,43 @@
 
-const socket=io();
+let map=L.map('map').setView([40.4093,49.8671],13)
 
-let map=L.map('map').setView([40.4093,49.8671],13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+maxZoom:19
+}).addTo(map)
 
-document.getElementById("rideBtn").onclick=()=>{
- socket.emit("ride-request",{lat:40.4093,lng:49.8671});
-};
+let pickupMarker=null
+let routeLine=null
 
-function sendMsg(){
- let msg=document.getElementById("msg").value;
- socket.emit("chat-message",msg);
+map.on("click",e=>{
+
+if(!pickupMarker){
+
+pickupMarker=L.marker(e.latlng).addTo(map)
+
+}else{
+
+if(routeLine) map.removeLayer(routeLine)
+
+routeLine=L.polyline([pickupMarker.getLatLng(),e.latlng],{color:"black"}).addTo(map)
+
+document.getElementById("driverCard").style.display="block"
+
+}
+
+})
+
+document.querySelector(".ride").onclick=()=>{
+
+setTimeout(()=>{
+
+document.getElementById("rating").style.display="block"
+
+},4000)
+
+}
+
+function closeRating(){
+
+document.getElementById("rating").style.display="none"
+
 }
