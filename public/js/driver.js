29764131b32c4ef5e39342432a29a100b35a9398
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-13 18:17:29 +0400

    Add files via upload

--- public/js/driver.js
+++ public/js/driver.js
@@ -1,9 +1,9 @@
 
-const socket=io();
+const socket=io()
 
-let map=L.map('map').setView([40.4093,49.8671],13);
+let map=L.map('map').setView([40.4093,49.8671],13)
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19}).addTo(map)
 
 function goOnline(){
 
@@ -12,8 +12,20 @@ function goOnline(){
   socket.emit("driver-location",{
    lat:pos.coords.latitude,
    lng:pos.coords.longitude
-  });
+  })
 
- });
+ })
+
+}
+
+socket.on("ride-offer",ride=>{
+
+ document.getElementById("ride").innerText="New Ride Request"
+
+})
+
+function finishRide(){
+
+ socket.emit("ride-finish",{status:"done"})
 
 }
