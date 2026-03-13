3cddb65e82969fdcf8fbb3f505150c37e46dd35e
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-13 18:13:47 +0400

    Add files via upload

--- public/js/passenger.js
+++ public/js/passenger.js
@@ -1,18 +1,38 @@
 
-const socket=io()
+const socket=io();
 
-let map=L.map('map').setView([40.4093,49.8671],13)
+let map=L.map('map').setView([40.4093,49.8671],13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19}).addTo(map)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19}).addTo(map);
 
-let pickupInput=document.getElementById("pickup")
-let dropInput=document.getElementById("drop")
+let driverMarkers={};
 
-new google.maps.places.Autocomplete(pickupInput)
-new google.maps.places.Autocomplete(dropInput)
+socket.on("drivers-update",drivers=>{
+
+ for(let id in drivers){
+
+  if(driverMarkers[id]){
+
+   driverMarkers[id].setLatLng([drivers[id].lat,drivers[id].lng]);
+
+  }else{
+
+   driverMarkers[id]=L.marker([drivers[id].lat,drivers[id].lng]).addTo(map);
+
+  }
+
+ }
+
+});
+
+let pickupInput=document.getElementById("pickup");
+let dropInput=document.getElementById("drop");
+
+new google.maps.places.Autocomplete(pickupInput);
+new google.maps.places.Autocomplete(dropInput);
 
 function requestRide(){
 
- socket.emit("ride-request",{lat:40.4093,lng:49.8671})
+ socket.emit("ride-request",{lat:40.4093,lng:49.8671});
 
 }
